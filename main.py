2a0052eaca64a21fb9dee9ea037aec3a1761e86b
bjavohir2197 <bjavohir2197@gmail.com>
2026-04-18 15:21:21 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,24 @@
+def royxatni_teskari_tartibda_chiqar(ro'yhat):
+    return ro'yhat[::-1]
+
+# Misol:
+ro'yhat = ["A", "B", "C", "D", "E"]
+print(royxatni_teskari_tartibda_chiqar(ro'yhat))
+```
+
+```python
+def royxatni_teskari_tartibda_chiqar(ro'yhat):
+    return list(reversed(ro'yhat))
+
+# Misol:
+ro'yhat = ["A", "B", "C", "D", "E"]
+print(royxatni_teskari_tartibda_chiqar(ro'yhat))
+```
+
+```python
+def royxatni_teskari_tartibda_chiqar(ro'yhat):
+    return ro'yhat[-1:0:-1]
+
+# Misol:
+ro'yhat = ["A", "B", "C", "D", "E"]
+print(royxatni_teskari_tartibda_chiqar(ro'yhat))
